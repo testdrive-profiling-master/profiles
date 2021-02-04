637b97caa2458bdf76b883d326ab9bc1e244883d
HyungKi Jeong <a>
2021-02-04 12:49:13 +0900

    change to gtkwave -> gtkwave-gtk2

--- Meitner/Profiles/common/view_sim_wave.bat
+++ Meitner/Profiles/common/view_sim_wave.bat
@@ -7,6 +7,6 @@ goto END
 
 :RUN_GTKWAVE
 @echo Opening Waveform file(%SIM_OUTPUT_FILE%)...
-start "View Waveform" "%TESTDRIVE_DIR%bin\msys64\mingw64\bin\gtkwave.exe" -r %TESTDRIVE_DIR%bin\gtkwave.ini -g %SIM_OUTPUT_FILE% %SIM_DO_FILE%
+start "View Waveform" "%TESTDRIVE_DIR%bin\msys64\mingw64\bin\gtkwave-gtk2.exe" -r %TESTDRIVE_DIR%bin\gtkwave.ini -g %SIM_OUTPUT_FILE% %SIM_DO_FILE%
 
 :END
