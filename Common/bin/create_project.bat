e348d7a0c375fe7c2ec9ed5b2dd4306c361352d8
HyungKi Jeong <a>
2021-04-15 11:16:46 +0900

    add more option string for create_project

--- Common/bin/create_project.bat
+++ Common/bin/create_project.bat
@@ -11,9 +11,9 @@ if "%1"=="" (
 	echo Usage : create_project project_type project_name
 	echo.
 	echo    project_type
-	echo        'cpp'         : C++ project
-	echo        'verilog'     : verilog project
-	echo    project_name      : making project name
+	echo        'c', 'c++', 'cpp'   : C++ project
+	echo        'v', 'verilog'      : verilog project
+	echo    project_name            : making project name
 	echo.
 	echo ex. create_project cpp test
 	goto EXIT
@@ -26,13 +26,22 @@ if exist %2 (
 	goto EXIT
 )
 
-if "%1"=="cpp" (
+setlocal
+set project_type=none
+
+if "%1"=="c"       set project_type=cpp
+if "%1"=="c++"     set project_type=cpp
+if "%1"=="cpp"     set project_type=cpp
+if "%1"=="v"       set project_type=verilog
+if "%1"=="verilog" set project_type=verilog
+
+if "%project_type%"=="cpp" (
 	mkdir %2
 	cp -rf %TESTDRIVE_PROFILE%Common/bin/project_template_cpp/. %2/
 	sed "s/Test/%2/" -i ./%2/.project
 	sed "s/Test/%2/" -i ./%2/Makefile
 	sed "s/Test/%2 project/" -i ./%2/.inception
-) else if "%1"=="verilog" (
+) else if "%project_type%"=="verilog" (
 	mkdir %2
 	cp -rf %TESTDRIVE_PROFILE%Common/bin/project_template_verilog/. %2/
 	mv ./%2/dpi_private/.src.cpp ./%2/dpi_private/%2.cpp
@@ -42,6 +51,7 @@ if "%1"=="cpp" (
 	echo *E: Project type does not exist : "%1"
 	goto SHOW_USAGE
 )
+endlocal
 echo Done.
 
 :EXIT
