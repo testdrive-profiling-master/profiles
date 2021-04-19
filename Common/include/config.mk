9d582623250d2e0fcb8094411dfcebdb7570c795
HyungKi Jeong <a>
2021-04-19 16:35:01 +0900

    add dep print.

--- Common/include/config.mk
+++ Common/include/config.mk
@@ -102,16 +102,19 @@ static:
 #########################################################################
 # Dependency
 %.d: %.c
+	@echo '- Dependency... : $<'
 	@set -e; rm -f $@; \
 	$(CC) -M $(CDEFS) $(CFLAGS) $(INC) $< > $@; \
 	sed -e 's,[^.]*.o:,$*.o:,g' -i $@
 
 %.d: %.cpp
+	@echo '- Dependency... : $<'
 	@set -e; rm -f $@; \
 	$(CXX) -M $(CDEFS) $(CFLAGS) -Weffc++ $(INC) $< > $@; \
 	sed -e 's,[^.]*.o:,$*.o:,g' -i $@
 
 %.d: %.cc
+	@echo '- Dependency... : $<'
 	@set -e; rm -f $@; \
 	$(CXX) -M $(CDEFS) $(CFLAGS) -Weffc++ $(INC) $< > $@; \
 	sed -e 's,[^.]*.o:,$*.o:,g' -i $@
