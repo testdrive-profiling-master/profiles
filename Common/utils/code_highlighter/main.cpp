640cdf0be1685f58be781e7087ee356edb56555a
Hyungki Jeong <clonextop@gmail.com>
2022-03-10 18:18:25 +0900

    add code_highlighter

---
+++ Common/utils/code_highlighter/main.cpp
@@ -0,0 +1,87 @@
+#include "ArgTable.h"
+#include "cstring.h"
+#include "source-highlight/sourcehighlight.h"
+#include "source-highlight/langmap.h"
+
+using namespace std;
+using namespace srchilite;
+
+int main(int argc, const char* argv[])
+{
+	ArgTable	main_arg_table("TestDrive code high-lighter. (build date : " __DATE__ ")");
+	main_arg_table.AddOptionString("input_lang", "cpp", NULL, "ilang", "input_lang", "input language identifier string");
+	main_arg_table.AddRemark(NULL, "default : \"cpp\"");
+	main_arg_table.AddOptionString("out_lang", "html", NULL, "olang", "out_lang", "output language identifier string");
+	main_arg_table.AddRemark(NULL, "default : \"html\"");
+	main_arg_table.AddOption("list", "l", NULL, "list up possible input code format");
+	main_arg_table.AddOption("line_number", "n", NULL, "Enable line number");
+	main_arg_table.AddOptionInt("start_number", 1, "i", NULL, "start_number", "set start line number");
+	main_arg_table.AddRemark(NULL, "default : 1");
+	main_arg_table.AddOptionString("style", "", "s", "style", "style", "style");
+	main_arg_table.AddOptionString("input", "", NULL, NULL, "input_file", "input file");
+	main_arg_table.AddRemark(NULL, "default : stdin");
+	main_arg_table.AddOptionString("output", "", NULL, NULL, "output_file", "output file");
+	main_arg_table.AddRemark(NULL, "default : stdout");
+
+	if(!main_arg_table.DoParse(argc, argv))
+		return 1;
+
+	// get base environment path
+	cstring	sBasePath	= GetCommonToolPath();
+	sBasePath.Append("bin/source-highlight-lang");
+
+	// get output language format
+	cstring	sOutLang	= main_arg_table.GetOptionString("out_lang");
+	if(!sOutLang.CompareBack(".outlang")) {	// correct file extension
+		sOutLang.Append(".outlang");
+	}
+	if(access(sOutLang.c_str(), F_OK)) {	// can't search on file path
+		sOutLang.insert(0, "/");
+		sOutLang.insert(0, sBasePath.c_str());
+		if(access(sOutLang.c_str(), F_OK)) {	// can't search on file path
+			LOGE("Can't find out-lang file '%s'.", main_arg_table.GetOptionString("out_lang"));
+			return 1;
+		}
+	}
+
+	// get input/output file name
+	cstring		sInput		= main_arg_table.GetOptionString("input");
+	cstring		sOutput		= main_arg_table.GetOptionString("output");
+	if(sInput == "stdin") sInput.clear();
+	if(sOutput == "stdout") sOutput.clear();
+
+	// get input language format
+	srchilite::LangMap langMap(sBasePath.c_str(), "lang.map");
+	string sInputLang	= langMap.getMappedFileNameFromFileName(main_arg_table.GetOptionString("input_lang"));
+	if(sInputLang.empty()) {
+		LOGE("Can't find input-lang : '%s'", main_arg_table.GetOptionString("input_lang"));
+		return 1;
+	}
+
+	if(main_arg_table.GetOption("list")) {
+		int iCount = 0;
+		printf("*** Available current input code format(%s/lang.map) list\n", sBasePath.c_str());
+		for(auto& i : langMap) {
+			printf(!(iCount%10) ? "\n\t" : (iCount ? ", " : ""));
+			printf("%s", i.first.c_str());
+			iCount++;
+		}
+		printf("\n");
+		return 0;
+	}
+
+	cstring		sStyle		= main_arg_table.GetOptionString("style");
+
+	{	// do right thing...
+		SourceHighlight sourceHighlight(sOutLang.c_str());
+		sourceHighlight.setDataDir(sBasePath.c_str());
+		sourceHighlight.setTabSpaces(4);
+		if(main_arg_table.GetOption("line_number")) {
+			sourceHighlight.setGenerateLineNumbers();
+			sourceHighlight.setLineNumberDigits(main_arg_table.GetOptionInt("start_number"));
+		}
+		if(sStyle.Length()) sourceHighlight.setStyleFile(sStyle.c_str());
+		sourceHighlight.highlight(sInput.c_str(), sOutput.c_str(), sInputLang);
+	}
+	return 0;
+}
