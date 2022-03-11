95ddcfdb70573acec13cf50c95d7742bb282da13
Hyungki Jeong <clonextop@gmail.com>
2022-03-11 17:07:44 +0900

    add verilog lang

--- Common/utils/code_highlighter/main.cpp
+++ Common/utils/code_highlighter/main.cpp
@@ -1,4 +1,5 @@
 #include "ArgTable.h"
+#include "TextFile.h"
 #include "cstring.h"
 #include "source-highlight/sourcehighlight.h"
 #include "source-highlight/langmap.h"
@@ -15,8 +16,6 @@ int main(int argc, const char* argv[])
 	main_arg_table.AddRemark(NULL, "default : \"html\"");
 	main_arg_table.AddOption("list", "l", NULL, "list up possible input code format");
 	main_arg_table.AddOption("line_number", "n", NULL, "Enable line number");
-	main_arg_table.AddOptionInt("start_number", 1, "i", NULL, "start_number", "set start line number");
-	main_arg_table.AddRemark(NULL, "default : 1");
 	main_arg_table.AddOptionString("style", "", "s", "style", "style", "style");
 	main_arg_table.AddOptionString("input", "", NULL, NULL, "input_file", "input file");
 	main_arg_table.AddRemark(NULL, "default : stdin");
@@ -47,8 +46,20 @@ int main(int argc, const char* argv[])
 	// get input/output file name
 	cstring		sInput		= main_arg_table.GetOptionString("input");
 	cstring		sOutput		= main_arg_table.GetOptionString("output");
+	int			iLineCount	= 0;
 	if(sInput == "stdin") sInput.clear();
 	if(sOutput == "stdout") sOutput.clear();
+
+	if(sInput.Length()) {	// get line count
+		TextFile	f;
+		if(!f.Open(sInput)) {	// can't search on file path
+			LOGE("Can't open input file. : '%s'", sInput.c_str());
+			return 1;
+		}
+		cstring sLine;
+		while(f.GetLine(sLine));
+		iLineCount	= f.LineNumber();
+	}
 
 	// get input language format
 	srchilite::LangMap langMap(sBasePath.c_str(), "lang.map");
@@ -78,7 +89,12 @@ int main(int argc, const char* argv[])
 		sourceHighlight.setTabSpaces(4);
 		if(main_arg_table.GetOption("line_number")) {
 			sourceHighlight.setGenerateLineNumbers();
-			sourceHighlight.setLineNumberDigits(main_arg_table.GetOptionInt("start_number"));
+			sourceHighlight.setLineNumberPad(' ');
+			sourceHighlight.setLineNumberDigits(
+					iLineCount < 10 ? 1 :
+					iLineCount < 100 ? 2 :
+					iLineCount < 1000 ? 3 : 4
+					);
 		}
 		if(sStyle.Length()) sourceHighlight.setStyleFile(sStyle.c_str());
 		sourceHighlight.highlight(sInput.c_str(), sOutput.c_str(), sInputLang);
