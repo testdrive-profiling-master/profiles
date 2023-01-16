f9324856fbb514c0e8a279616deeb176d98a3bc5
HyungKi Jeong <hyungki@mobilint.co>
2023-01-16 11:01:58 +0900

    fix makefile default config.

--- Common/include/config.mk
+++ Common/include/config.mk
@@ -68,9 +68,9 @@ endif
 
 ifeq ($(EXTRA_CFLAGS),)
 ifdef USE_DEBUG
-	EXTRA_CFLAGS	:= -O0 -std=c++17 -DDEBUG -g
+	EXTRA_CFLAGS	:= -O0 -std=c++14 -DDEBUG -g
 else
-	EXTRA_CFLAGS	:= -O3 -std=c++17 -DNDEBUG
+	EXTRA_CFLAGS	:= -O3 -std=c++14 -DNDEBUG
 endif
 endif
 
