e6a2989fc9faf7d21ad77f33e72af5e7bf232ec6
HyungKi Jeong <hyungki@mobilint.co>
2023-05-24 19:32:40 +0900

    fix cstring:FindVariableString to ignore string in string expression

--- Common/utils/framework/cstring.cpp
+++ Common/utils/framework/cstring.cpp
@@ -2,22 +2,22 @@
 // Copyright (c) 2013 ~ 2023. HyungKi Jeong(clonextop@gmail.com)
 // Freely available under the terms of the 3-Clause BSD License
 // (https://opensource.org/licenses/BSD-3-Clause)
-// 
+//
 // Redistribution and use in source and binary forms,
 // with or without modification, are permitted provided
 // that the following conditions are met:
-// 
+//
 // 1. Redistributions of source code must retain the above copyright notice,
 //    this list of conditions and the following disclaimer.
-// 
+//
 // 2. Redistributions in binary form must reproduce the above copyright notice,
 //    this list of conditions and the following disclaimer in the documentation
 //    and/or other materials provided with the distribution.
-// 
+//
 // 3. Neither the name of the copyright holder nor the names of its contributors
 //    may be used to endorse or promote products derived from this software
 //    without specific prior written permission.
-// 
+//
 // THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
 // AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
 // THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
@@ -29,7 +29,7 @@
 // STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
 // ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY
 // OF SUCH DAMAGE.
-// 
+//
 // Title : utility framework
 // Rev.  : 4/28/2023 Fri (clonextop@gmail.com)
 //================================================================================
@@ -288,6 +288,13 @@ static bool __RetrieveBracketString(const char* sSearch, int& iPos, const char* 
 
 		case '\\':
 			iPos++;
+
+			if(bString) {
+				if(!sSearch[iPos] == '\"') {	// string in string
+					iPos++;
+					break;
+				}
+			}
 
 			if(!sSearch[iPos]) return false;
 
