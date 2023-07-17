737f411f4bc139800a77372a6dfeeb9e6e5d1586
HyungKi Jeong <hyungki@mobilint.co>
2023-07-17 19:20:42 +0900

    fix f32 adder

--- Meitner/Application/Example/Testbench/FPU_Adder/HDL/FPU_F32_Adder.v
+++ Meitner/Application/Example/Testbench/FPU_Adder/HDL/FPU_F32_Adder.v
@@ -40,20 +40,80 @@ module FPU_F32_Adder(
 	output	[31:0]		O	              	//Outputs in the format of IEEE-754 Representation.
 );
 
-wire operation_add;
-wire Comp_enable;
-wire output_sign;
+wire	[31:0]			a, b;
+// A must greater than B
+assign {a, b} = (A[30:0] < B[30:0]) ? {B, A} : {A, B};
 
-wire [31:0] operand_a,operand_b;
-wire [23:0] significand_a,significand_b;
-wire [7:0] exponent_diff;
+// dispatch floating point component
+wire					sign_a, sign_b;
+wire	[7:0]			exp_a, exp_b;
+wire	[22:0]			mantissa_a, mantissa_b;
+
+assign	{sign_a, exp_a, mantissa_a}		= a;
+assign	{sign_b, exp_b, mantissa_b}		= b;
+
+// significand
+wire	[23:0]			significand_a	= {|exp_a, mantissa_a};
+wire	[23:0]			significand_b	= {|exp_b, mantissa_b};
+
+// sub operation?
+wire	operation_sub	= A[31] ^ B[31];
+
+// output sign
+wire	sign_o			= sign_a;
+
+// exponential difference
+wire	[7:0]			exp_diff		= exp_a - exp_b;
+
+// significand_b shifted
+wire	[24:0]			significand_b_shifted = (
+	(exp_diff == 8'd0 ) ? {significand_b, 1'b0} :
+	(exp_diff == 8'd1 ) ? { 1'b0, significand_b} :
+	(exp_diff == 8'd2 ) ? { 2'b0, significand_b[23:1]} :
+	(exp_diff == 8'd3 ) ? { 3'b0, significand_b[23:2]} :
+	(exp_diff == 8'd4 ) ? { 4'b0, significand_b[23:3]} :
+	(exp_diff == 8'd5 ) ? { 5'b0, significand_b[23:4]} :
+	(exp_diff == 8'd6 ) ? { 6'b0, significand_b[23:5]} :
+	(exp_diff == 8'd7 ) ? { 7'b0, significand_b[23:6]} :
+	(exp_diff == 8'd8 ) ? { 8'b0, significand_b[23:7]} :
+	(exp_diff == 8'd9 ) ? { 9'b0, significand_b[23:8]} :
+	(exp_diff == 8'd10) ? {10'b0, significand_b[23:9]} :
+	(exp_diff == 8'd11) ? {11'b0, significand_b[23:10]} :
+	(exp_diff == 8'd12) ? {12'b0, significand_b[23:11]} :
+	(exp_diff == 8'd13) ? {13'b0, significand_b[23:12]} :
+	(exp_diff == 8'd14) ? {14'b0, significand_b[23:13]} :
+	(exp_diff == 8'd15) ? {15'b0, significand_b[23:14]} :
+	(exp_diff == 8'd16) ? {16'b0, significand_b[23:15]} :
+	(exp_diff == 8'd17) ? {17'b0, significand_b[23:16]} :
+	(exp_diff == 8'd18) ? {18'b0, significand_b[23:17]} :
+	(exp_diff == 8'd19) ? {19'b0, significand_b[23:18]} :
+	(exp_diff == 8'd20) ? {20'b0, significand_b[23:19]} :
+	(exp_diff == 8'd21) ? {21'b0, significand_b[23:20]} :
+	(exp_diff == 8'd22) ? {22'b0, significand_b[23:21]} :
+	(exp_diff == 8'd23) ? {23'b0, significand_b[23:22]} :
+	(exp_diff == 8'd24) ? {24'b0, significand_b[23]} :
+	/* default */         25'd0
+);
+
+//----------------------------------------------------------
+// adder module
+wire	[24:0]		significand_add 	= {1'b0, significand_a} + {1'b0, significand_b_shifted[24:1]};
+wire	[24:0]		significand_add_o	= sign_o ? significand_add : (significand_add + {24'd0, significand_b_shifted[0]});
+
+wire	[22:0]		mantissa_add		= significand_add_o[24] ? significand_add_o[23:1] : significand_add_o[22:0];
+wire	[30:23]		exp_add				= significand_add_o[24] ? (1'b1 + exp_a) : exp_a;
+
+//----------------------------------------------------------
+// sub module
+wire	[24:0]		significand_sub 	= {significand_a, 1'b0} - {significand_b_shifted};
+wire	[23:0]		significand_sub_o	= sign_o ? significand_sub[24:1] : (significand_sub[24:1] + {23'd0, significand_sub[0]});
+/*wire	[22:0]		mantissa_sub		= (
+
+);*/
 
 
 wire [23:0] significand_b_add_sub;
 wire [7:0] exponent_b_add_sub;
-
-wire [24:0] significand_add;
-wire [30:0] add_sum;
 
 wire [23:0] significand_sub_complement;
 wire [24:0] significand_sub;
@@ -61,50 +121,30 @@ wire [30:0] sub_diff;
 wire [24:0] subtraction_diff;
 wire [7:0] exponent_sub;
 
-//for operations always operand_a must not be less than B
-assign {Comp_enable,operand_a,operand_b} = (A[30:0] < B[30:0]) ? {1'b1, B, A} : {1'b0, A, B};
-
 //Exception flag sets 1 if either one of the exponent is 255.
-assign Exception			= (&operand_a[30:23]) | (&operand_b[30:23]);
+assign Exception			= (&a[30:23]) | (&b[30:23]);
 
-assign output_sign			= operand_a[31] ;
-assign operation_add		= ~(operand_a[31] ^ operand_b[31]);
+//Shifting significand_b according to exp_diff
+assign significand_b_add_sub = significand_b >> exp_diff;
 
-//Assigining significand values according to Hidden Bit.
-//If exponent is equal to zero then hidden bit will be 0 for that respective significand else it will be 1
-assign significand_a = (|operand_a[30:23]) ? {1'b1,operand_a[22:0]} : {1'b0,operand_a[22:0]};
-assign significand_b = (|operand_b[30:23]) ? {1'b1,operand_b[22:0]} : {1'b0,operand_b[22:0]};
-
-//Evaluating Exponent Difference
-assign exponent_diff = operand_a[30:23] - operand_b[30:23];
-
-//Shifting significand_b according to exponent_diff
-assign significand_b_add_sub = significand_b >> exponent_diff;
-
-assign exponent_b_add_sub = operand_b[30:23] + exponent_diff;
+assign exponent_b_add_sub = b[30:23] + exp_diff;
 
 //Checking exponents are same or not
-wire perform = (operand_a[30:23] == exponent_b_add_sub);
+wire perform = (a[30:23] == exponent_b_add_sub);
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////
 //------------------------------------------------ADD BLOCK------------------------------------------//
 
-assign significand_add = (perform & operation_add) ? (significand_a + significand_b_add_sub) : 25'd0;
 
-//Result will be equal to Most 23 bits if carry generates else it will be Least 22 bits.
-assign add_sum[22:0] = significand_add[24] ? significand_add[23:1] : significand_add[22:0];
-
-//If carry generates in sum value then exponent must be added with 1 else feed as it is.
-assign add_sum[30:23] = significand_add[24] ? (1'b1 + operand_a[30:23]) : operand_a[30:23];
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////
 //------------------------------------------------SUB BLOCK------------------------------------------//
 
-assign significand_sub_complement = (perform & !operation_add) ? ~(significand_b_add_sub) + 24'd1 : 24'd0 ;
+assign significand_sub_complement = (perform & operation_sub) ? ~(significand_b_add_sub) + 24'd1 : 24'd0 ;
 
 assign significand_sub = perform ? (significand_a + significand_sub_complement) : 25'd0;
 
-FPU_F32_PriorityEncoder pe(significand_sub, operand_a[30:23], subtraction_diff, exponent_sub);
+FPU_F32_PriorityEncoder pe(significand_sub, a[30:23], subtraction_diff, exponent_sub);
 
 assign sub_diff[30:23] = exponent_sub;
 
@@ -114,6 +154,6 @@ assign sub_diff[22:0] = subtraction_diff[22:0];
 //-------------------------------------------------OUTPUT--------------------------------------------//
 
 //If there is no exception and operation will evaluate
-assign O = Exception ? 32'b0 : ((!operation_add) ? {output_sign, sub_diff} : {output_sign, add_sum});
+assign O = Exception ? 32'b0 : (operation_sub ? {sign_o, sub_diff} : {sign_o, exp_add, mantissa_add});
 
 endmodule
