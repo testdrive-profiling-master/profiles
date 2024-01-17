959d99550e861cd0dbb703ecfcbdab1850819cad
HyungKi Jeong <clonextop@gmail.com>
2024-01-17 17:21:45 +0900

    codegen 에서 VL_Release::All 에 의해서 Script 소멸자 호출이 안되었다.

--- Common/utils/codegen/main.cpp
+++ Common/utils/codegen/main.cpp
@@ -1,23 +1,23 @@
 //================================================================================
-// Copyright (c) 2013 ~ 2023. HyungKi Jeong(clonextop@gmail.com)
+// Copyright (c) 2013 ~ 2024. HyungKi Jeong(clonextop@gmail.com)
 // Freely available under the terms of the 3-Clause BSD License
 // (https://opensource.org/licenses/BSD-3-Clause)
-// 
+//
 // Redistribution and use in source and binary forms,
 // with or without modification, are permitted provided
 // that the following conditions are met:
-// 
+//
 // 1. Redistributions of source code must retain the above copyright notice,
 //    this list of conditions and the following disclaimer.
-// 
+//
 // 2. Redistributions in binary form must reproduce the above copyright notice,
 //    this list of conditions and the following disclaimer in the documentation
 //    and/or other materials provided with the distribution.
-// 
+//
 // 3. Neither the name of the copyright holder nor the names of its contributors
 //    may be used to endorse or promote products derived from this software
 //    without specific prior written permission.
-// 
+//
 // THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
 // AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
 // THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
@@ -29,9 +29,9 @@
 // STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
 // ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY
 // OF SUCH DAMAGE.
-// 
-// Title : make code application
-// Rev.  : 1/26/2023 Thu (clonextop@gmail.com)
+//
+// Title : TestDrive codegen project
+// Rev.  : 1/17/2024 Wed (clonextop@gmail.com)
 //================================================================================
 #include "Script.h"
 #include "ArgTable.h"
@@ -83,7 +83,6 @@ int main(int argc, const char* argv[])
 	// set UTF-8 mode
 	SetConsoleOutputCP(65001);
 #endif
-
 	// get options
 	__sLuaFileName	= main_arg_table.GetOptionFile("lua_file");
 	LuaFile::SetKeyCode(main_arg_table.GetOptionString("key"));
@@ -150,8 +149,7 @@ int main(int argc, const char* argv[])
 
 		if(sc.Run(__sLuaFileName))
 			iRet	= 0;
-
-		VL::ReleaseAll();
 	}
+	VL::ReleaseAll();
 	return iRet;
 }
