4d0292cb7fadda8c86821295262ddc5c50bba292
HyungKi Jeong <clonextop@gmail.com>
2024-03-11 12:24:58 +0900

    add cstdint to SDTInterface.h

--- Common/include/STDInterface.h
+++ Common/include/STDInterface.h
@@ -1,23 +1,23 @@
 //================================================================================
-// Copyright (c) 2013 ~ 2023. HyungKi Jeong(clonextop@gmail.com)
+// Copyright (c) 2013 ~ 2024. HyungKi Jeong(clonextop@gmail.com)
 // Freely available under the terms of the 3-Clause BSD License
 // (https://opensource.org/licenses/BSD-3-Clause)
-// 
+//
 // Redistribution and use in source and binary forms,
 // with or without modification, are permitted provided
 // that the following conditions are met:
-// 
+//
 // 1. Redistributions of source code must retain the above copyright notice,
 //    this list of conditions and the following disclaimer.
-// 
+//
 // 2. Redistributions in binary form must reproduce the above copyright notice,
 //    this list of conditions and the following disclaimer in the documentation
 //    and/or other materials provided with the distribution.
-// 
+//
 // 3. Neither the name of the copyright holder nor the names of its contributors
 //    may be used to endorse or promote products derived from this software
 //    without specific prior written permission.
-// 
+//
 // THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
 // AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
 // THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
@@ -29,64 +29,77 @@
 // STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
 // ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY
 // OF SUCH DAMAGE.
-// 
+//
 // Title : Common profiles
-// Rev.  : 2/3/2023 Fri (clonextop@gmail.com)
+// Rev.  : 3/11/2024 Mon (clonextop@gmail.com)
 //================================================================================
 #ifndef __STD_INTERFACE_H__
 #define __STD_INTERFACE_H__
-#include <stdio.h>
+#include <cstdint>
 #include <limits.h>
+#include <stdio.h>
 
 #ifdef WIN32
-#ifndef WIN32_LEAN_AND_MEAN
-#define WIN32_LEAN_AND_MEAN
-#endif
-#include <windows.h>
+#	ifndef WIN32_LEAN_AND_MEAN
+#		define WIN32_LEAN_AND_MEAN
+#	endif
+#	include <windows.h>
 #else
-#ifndef PVOID
-typedef void* PVOID;
-#endif
-#if !defined(INT64)
-#define INT64		long long
-#endif
-#if !defined(UINT64)
-#define UINT64		unsigned INT64
-#endif
-#if !defined(LONGLONG)
-#define LONGLONG	long long
-#endif
-#if !defined(LONG)
-#define LONG		long
-#endif
-#if !defined(DWORD)
-#define DWORD		unsigned int
-#endif
-#if !defined(WORD)
-#define WORD		unsigned short
-#endif
-#if !defined(BYTE)
-#define BYTE		unsigned char
-#endif
-#if !defined(BOOL)
-#define BOOL		DWORD
-#define TRUE		1
-#define FALSE		0
-#endif
-#if !defined(NULL)
-#define	NULL		0
-#endif
+#	ifndef PVOID
+typedef void *PVOID;
+#	endif
+#	if !defined(INT64)
+#		define INT64 long long
+#	endif
+#	if !defined(UINT64)
+#		define UINT64 unsigned INT64
+#	endif
+#	if !defined(LONGLONG)
+#		define LONGLONG long long
+#	endif
+#	if !defined(LONG)
+#		define LONG long
+#	endif
+#	if !defined(DWORD)
+#		define DWORD unsigned int
+#	endif
+#	if !defined(WORD)
+#		define WORD unsigned short
+#	endif
+#	if !defined(BYTE)
+#		define BYTE unsigned char
+#	endif
+#	if !defined(BOOL)
+#		define BOOL  DWORD
+#		define TRUE  1
+#		define FALSE 0
+#	endif
+#	if !defined(NULL)
+#		define NULL 0
+#	endif
 
-#if defined(KERNEL_64BIT)
+#	if defined(KERNEL_64BIT)
 typedef INT64 KPTR;
-#else
+#	else
 typedef DWORD KPTR;
-#endif
+#	endif
 #endif
 
 // Safe memory deletion
-#define SAFE_DELETE(x)			if(x){delete x;			(x) = NULL;}
-#define SAFE_DELETE_ARRAY(x)	if(x){delete [] x;		(x) = NULL;}
-#define SAFE_RELEASE(x)			if(x){(x)->Release();	(x) = NULL;}
+#define SAFE_DELETE(x)                                                                                                 \
+	if (x) {                                                                                                           \
+		delete x;                                                                                                      \
+		(x) = NULL;                                                                                                    \
+	}
+#define SAFE_DELETE_ARRAY(x)                                                                                           \
+	if (x) {                                                                                                           \
+		delete[] x;                                                                                                    \
+		(x) = NULL;                                                                                                    \
+	}
+#define SAFE_RELEASE(x)                                                                                                \
+	if (x) {                                                                                                           \
+		(x)->Release();                                                                                                \
+		(x) = NULL;                                                                                                    \
+	}
 
-#endif//__STD_INTERFACE_H__
+#endif //__STD_INTERFACE_H__
