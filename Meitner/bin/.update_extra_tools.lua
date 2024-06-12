cb8771cba8893e0f174df7348ea27d5816c9e9b2
HyungKi Jeong <clonextop@gmail.com>
2024-06-12 12:22:36 +0900

    move required package list to startup lua script

--- Meitner/bin/.update_extra_tools.lua
+++ Meitner/bin/.update_extra_tools.lua
@@ -1,5 +1,2 @@
-RequireMingwPackage("mingw-w64-ucrt-x86_64-graphviz")
-RequireMingwPackage("mingw-w64-ucrt-x86_64-source-highlight")
-RequireMingwPackage("mingw-w64-ucrt-x86_64-lua-luarocks")
-RequireMingwPackage("mingw-w64-ucrt-x86_64-pdf2djvu")
-RequireMingwPackage("mingw-w64-ucrt-x86_64-qrencode")
+-- Requred package list
+--RequireMingwPackage("mingw-w64-ucrt-x86_64-graphviz")
