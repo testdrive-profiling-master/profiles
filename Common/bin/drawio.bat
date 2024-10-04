5e3214f0110eb9cee8cc8c94179fb256017c3c1a
HyungKi Jeong <clonextop@gmail.com>
2024-10-04 10:32:31 +0900

    재설치 기능 추가

--- Common/bin/drawio.bat
+++ Common/bin/drawio.bat
@@ -1,4 +1,10 @@
 @echo off
+if "%1" == "install" (
+	rm -rf "%TESTDRIVE_PROFILE%\Common\bin\drawio"
+	codegen check_drawio
+	goto END
+)
+
 codegen check_drawio
 
 Echo.%* | findstr /C:"-">nul && (
@@ -6,3 +12,4 @@ Echo.%* | findstr /C:"-">nul && (
 ) || (
     start /B "draw.io" "%TESTDRIVE_PROFILE%\Common\bin\drawio\draw.io.exe" %*
 )
+:END
