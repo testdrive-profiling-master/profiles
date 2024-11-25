208e5bd81094e5efb0b0df079bcc04ef17e6f2d5
HyungKi Jeong <clonextop@gmail.com>
2024-11-25 15:58:28 +0900

    update makefile for webgui

--- Common/utils/include/util_gui.mk
+++ Common/utils/include/util_gui.mk
@@ -17,7 +17,7 @@ endif
 TARGET_DEP		:= $(TARGET_DEP) $(TESTDRIVE_PROFILE)Common/utils/lib/libUtilFramework.a $(TESTDRIVE_PROFILE)Common/utils/lib/libwebgui.a
 CDEFS			:= $(CDEFS) -D_WIN32_IE=0x605 -DWINVER=0x605
 INC				:= $(INC) -I$(TESTDRIVE_PROFILE)Common/utils/framework -I$(TESTDRIVE_PROFILE)Common/utils/include -I$(TESTDRIVE_PROFILE)Common/utils/lib_src
-LIBDIR			:= -L$(TESTDRIVE_PROFILE)Common/utils/lib -lwebgui -lshlwapi -lole32 -lversion -lmicrohttpd -lWs2_32 -lgdi32 -ljsoncpp -lUtilFramework -liconv $(LIBDIR)
+LIBDIR			:= -L$(TESTDRIVE_PROFILE)Common/utils/lib -lwebgui -lshlwapi -lole32 -lversion -lmicrohttpd -lWs2_32 -lgdi32 `pkg-config --libs-only-l jsoncpp` -lUtilFramework -liconv $(LIBDIR)
 
 ifndef USE_DEBUG
 LDFLAGS			:= $(LDFLAGS) -mwindows
