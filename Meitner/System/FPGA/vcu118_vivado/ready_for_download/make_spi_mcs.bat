2320689c05feaf32dea78c64f30b30802eca5e83
HyungKi Jeong <clonextop@gmail.com>
2025-03-04 15:37:30 +0900

    update vivado project to 2024.2

--- Meitner/System/FPGA/vcu118_vivado/ready_for_download/make_spi_mcs.bat
+++ Meitner/System/FPGA/vcu118_vivado/ready_for_download/make_spi_mcs.bat
@@ -1,4 +1,4 @@
-call D:\\Xilinx\\Vivado\\2023.2\\bin\\vivado.bat -mode batch -source make_spi_mcs.tcl
+call D:\\Xilinx\\Vivado\\2024.2\\bin\\vivado.bat -mode batch -source make_spi_mcs.tcl
 if exist *isWriteableTest*.tmp del /F *isWriteableTest*.tmp
 if exist *pid*.* del /F *pid*.*
 if exist vivado_*.str del /F vivado_*.str
