ae8f0ea3df95049865ec3b6ca85ac3cd977803e9
HyungKi Jeong <clonextop@gmail.com>
2025-08-22 14:28:50 +0900

    fix misc.

--- Common/bin/codegen/llama.lua
+++ Common/bin/codegen/llama.lua
@@ -10,32 +10,34 @@ if (Arg:DoParse() == false) then
 	return
 end
 
-if Arg:GetOption("clear") then
-	exec("del /Q %LOCALAPPDATA%\\llama.cpp\\*")
-	LOGI("All models are deleted.")
-	os.exit(0)
-end
-
 local iLayerCount = Arg:GetOptionInt("layers")
+local sDefaultModel = "unsloth/gemma-3-4b-it-GGUF"
 
 -- get model name from system configuration
 local model_name = String()
 model_name:GetEnvironment("MODEL@LLAMA")
 if model_name:Length() == 0 then
-	model_name.s = "unsloth/gemma-3-4b-it-GGUF" -- default model
+	model_name.s = sDefaultModel -- default model
 end
 
 do	-- update from argument, if existed
 	local sModel = Arg:GetOptionString("model")
 	
 	if Arg:GetOption("reset") then
-		sModel = "unsloth/gemma-3-4b-it-GGUF" -- default model
+		LOGI("Reset to default LLVM model. : " .. sDefaultModel)
+		sModel = sDefaultModel -- default model
 	end
 
 	if string.len(sModel) > 0 then
 		model_name.s = sModel
 		model_name:SetEnvironment("MODEL@LLAMA")
 	end
+end
+
+if Arg:GetOption("clear") then
+	exec("del /Q %LOCALAPPDATA%\\llama.cpp\\*")
+	LOGI("All models are deleted.")
+	os.exit(0)
 end
 
 -- install library if required
