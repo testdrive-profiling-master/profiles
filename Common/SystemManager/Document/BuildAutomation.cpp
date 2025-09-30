bab5b5c7f6d461b5d1ac3f90884078420e182bea
HyungKi Jeong <clonextop@gmail.com>
2025-09-30 16:47:06 +0900

    add clang-format with .cc

--- Common/SystemManager/Document/BuildAutomation.cpp
+++ Common/SystemManager/Document/BuildAutomation.cpp
@@ -31,7 +31,7 @@
 // OF SUCH DAMAGE.
 //
 // Title : System manager
-// Rev.  : 8/25/2025 Mon (clonextop@gmail.com)
+// Rev.  : 9/30/2025 Tue (clonextop@gmail.com)
 //================================================================================
 #include "BuildAutomation.h"
 
@@ -166,8 +166,8 @@ void BuildAutomation::DoCheck(DWORD command, LPCTSTR sFileName)
 
 				if (m_sCodingStyle.GetAt(0) &&
 					(sName.Find(_T(".h"), sName.GetLength() - 2) > 0 || sName.Find(_T(".c"), sName.GetLength() - 2) > 0 ||
-					 sName.Find(_T(".cpp"), sName.GetLength() - 4) > 0 || sName.Find(_T(".java"), sName.GetLength() - 5) > 0 ||
-					 sName.Find(_T(".inl"), sName.GetLength() - 4) > 0)) {
+					 sName.Find(_T(".cpp"), sName.GetLength() - 4) > 0 || sName.Find(_T(".cc"), sName.GetLength() - 3) > 0 ||
+					 sName.Find(_T(".java"), sName.GetLength() - 5) > 0 || sName.Find(_T(".inl"), sName.GetLength() - 4) > 0)) {
 				USE_CLANG_FORMAT:
 					CString sArg;
 					CString sClangFormatPath = g_pSystem->RetrieveFullPath(_T("%TESTDRIVE_DIR%bin\\msys64\\ucrt64\\bin\\clang-format.exe"));
