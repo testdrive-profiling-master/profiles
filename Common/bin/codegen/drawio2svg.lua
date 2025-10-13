5d00f02381f0fd1d403cb0a4d23d615f7a2cc2e5
HyungKi Jeong <clonextop@gmail.com>
2025-10-13 17:29:30 +0900

    draw.io : page number start with 1

--- Common/bin/codegen/drawio2svg.lua
+++ Common/bin/codegen/drawio2svg.lua
@@ -35,15 +35,10 @@ sProfilePath:Append("common/bin/")
 exec(".run \"" .. sProfilePath.s .. "drawio/draw.io.exe\" -x -f xml -u -o \"" .. in_file_name .. ".drawio2svg.xml\" \"" .. in_file_name .. "\"")
 
 local	page_list	= {}
-local	page_count	= 0
+local	page_count	= 1
 do
 	local xml = DocXML()
 	do	-- read from file
-		local xml_file_name		= String(in_file_name)
-		xml_file_name:CutFront("/", true)
-		xml_file_name:CutFront("\\", true)
-		xml_file_name:DeleteBack("odg")
-		xml_file_name:Append("xml")
 		if xml:LoadFromFile(in_file_name .. ".drawio2svg.xml") == false then
 			exec("rm -f \"" .. in_file_name .. ".drawio2svg.xml\"")
 			ERROR("SVGInformation check is failed.")
@@ -55,6 +50,9 @@ do
 	local child = node:child("diagram")
 	
 	while child:empty() == false do
+		if page_list[child:get_attribute("name")] ~= nil then
+			ERROR("Duplicated page name on " .. in_file_name .." : '" .. child:get_attribute("name") .. "'")
+		end
 		page_list[child:get_attribute("name")]	= page_count
 		page_count	= page_count + 1	-- start at '0'
 		if #page_name == 0 then	-- default page name (first page)
