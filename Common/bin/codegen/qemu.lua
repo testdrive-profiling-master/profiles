7207763ee58d8ecfc03b93eace27015f44ea5fb7
HyungKi Jeong <clonextop@gmail.com>
2026-04-06 22:22:25 +0900

    qemu devel 추가

--- Common/bin/codegen/qemu.lua
+++ Common/bin/codegen/qemu.lua
@@ -1,8 +1,9 @@
 local Arg = ArgTable("QEMU manager")
 
 Arg:AddOptionString		("cmd", nil, nil, nil, "command", "QEMU command")
-Arg:AddRemark			(nil, "install : reinstall QEMU")
-Arg:AddRemark			(nil, "run     : run QEMU")
+Arg:AddRemark			(nil, "install  : install QEMU binaries")
+Arg:AddRemark			(nil, "devel    : Prepare QEMU devel. project")
+Arg:AddRemark			(nil, "run      : run QEMU")
 
 
 if (Arg:DoParse() == false) then
@@ -20,10 +21,18 @@ local profile_path = String()
 profile_path:GetEnvironment("TESTDRIVE_PROFILE")
 profile_path = profile_path.s .. "Common/bin/"
 
--- check EDK2 tool
+-- Prepare QEMU development project
+if cmd == "devel" then
+	if lfs.IsExist("qemu_testdrive") == false then
+		run("git clone https://github.com/testdrive-profiling-master/qemu_testdrive.git qemu_testdrive")
+	end
+	os.exit(0)
+end
+
+-- check QEMU for TestDrive tool
 if (lfs.IsExist(profile_path .. "qemu-system-x86_64.exe") == false) or (cmd == "install") then
 	LOGI("Installing QEMU for TestDrive...\n")
-	if lfs.IsExist("qemu_src") == false then
+	if lfs.IsExist("qemu_testdrive") == false then
 		run("git clone https://github.com/testdrive-profiling-master/qemu_testdrive.git qemu_testdrive")
 	end
 	-- configure
