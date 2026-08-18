84476f22a70c3fd6bdbd6daa067fe3fe693cba42
HyungKi Jeong <clonextop@gmail.com>
2026-08-18 17:46:05 +0900

    log 적용하여, segment led 출력

--- Meitner/Profiles/FPGA_StarterKit/Document/RegmapNumericDisplay.cpp
+++ Meitner/Profiles/FPGA_StarterKit/Document/RegmapNumericDisplay.cpp
@@ -1,23 +1,23 @@
 //================================================================================
-// Copyright (c) 2013 ~ 2022. HyungKi Jeong(clonextop@gmail.com)
+// Copyright (c) 2013 ~ 2026. HyungKi Jeong(clonextop@gmail.com)
 // Freely available under the terms of the 3-Clause BSD License
 // (https://opensource.org/licenses/BSD-3-Clause)
-// 
+//
 // Redistribution and use in source and binary forms,
 // with or without modification, are permitted provided
 // that the following conditions are met:
-// 
+//
 // 1. Redistributions of source code must retain the above copyright notice,
 //    this list of conditions and the following disclaimer.
-// 
+//
 // 2. Redistributions in binary form must reproduce the above copyright notice,
 //    this list of conditions and the following disclaimer in the documentation
 //    and/or other materials provided with the distribution.
-// 
+//
 // 3. Neither the name of the copyright holder nor the names of its contributors
 //    may be used to endorse or promote products derived from this software
 //    without specific prior written permission.
-// 
+//
 // THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
 // AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
 // THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
@@ -29,22 +29,20 @@
 // STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
 // ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY
 // OF SUCH DAMAGE.
-// 
+//
 // Title : Starter Kit document
-// Rev.  : 1/4/2022 Tue (clonextop@gmail.com)
+// Rev.  : 8/18/2026 Tue (clonextop@gmail.com)
 //================================================================================
 #include "RegmapNumericDisplay.h"
 
 RegmapNumericDisplay::RegmapNumericDisplay(void) : Regmap(_T("NUM"))
 {
-	m_pNum		= &m_pReg->numeric_display;
+	m_pNum = &m_pReg->numeric_display;
 	memset(m_fSegments, 0, sizeof(m_fSegments));
-	m_fMid		= 0;
+	m_fMid = 0;
 }
 
-RegmapNumericDisplay::~RegmapNumericDisplay(void)
-{
-}
+RegmapNumericDisplay::~RegmapNumericDisplay(void) {}
 
 BOOL RegmapNumericDisplay::OnUpdate(void)
 {
@@ -53,7 +51,7 @@ BOOL RegmapNumericDisplay::OnUpdate(void)
 
 void RegmapNumericDisplay::OnBroadcast(LPVOID pData)
 {
-	if(!pData) {
+	if (!pData) {
 		UpdateData();
 	}
 }
@@ -65,64 +63,60 @@ BOOL RegmapNumericDisplay::OnCommand(LPCTSTR lpszURL)
 
 bool RegmapNumericDisplay::UpdateData(void)
 {
-	bool	bRet	= false;
+	bool bRet = false;
 
 	// seven segments
-	for(int i = 0; i < 4; i++) {
-		bool	bUpdate	= false;
+	for (int i = 0; i < 4; i++) {
+		bool bUpdate = false;
 
-		for(int t = 0; t < 8; t++) {
-			ACCUMULATE_DATA	post, recv;
-			post.m	= m_pNum->num[i].segment[t].post.m;
-			recv.m	= m_pNum->num[i].segment[t].recv.m;
-			DWORD	dwClock	= post.clk - recv.clk;
-			DWORD	dwAcc	= post.acc - recv.acc;
-			m_pNum->num[i].segment[t].recv.m	= post.m;
+		for (int t = 0; t < 8; t++) {
+			ACCUMULATE_DATA post, recv;
+			post.m							 = m_pNum->num[i].segment[t].post.m;
+			recv.m							 = m_pNum->num[i].segment[t].recv.m;
+			DWORD dwClock					 = post.clk - recv.clk;
+			DWORD dwAcc						 = post.acc - recv.acc;
+			m_pNum->num[i].segment[t].recv.m = post.m;
 
-			if(!dwClock) return false;
+			if (!dwClock)
+				return false;
 
-			float	val	= (double)dwAcc	/ dwClock;
+			float val = (log10(((double)dwAcc / dwClock) * 10) + 3) / 4;
+			if (val < 0)
+				val = 0;
 
-			if(m_fSegments[i][t] != val) {
+			if (m_fSegments[i][t] != val) {
 				m_fSegments[i][t] = val;
-				bUpdate	= true;
+				bUpdate			  = true;
 			}
 		}
 
-		if(bUpdate) {
-			g_pHtml->CallJScript(_T("SetNumericDisplay(%d,%.2f,%.2f,%.2f,%.2f,%.2f,%.2f,%.2f,%.2f)"),
-								 i,
-								 m_fSegments[i][0],
-								 m_fSegments[i][1],
-								 m_fSegments[i][2],
-								 m_fSegments[i][3],
-								 m_fSegments[i][4],
-								 m_fSegments[i][5],
-								 m_fSegments[i][6],
-								 m_fSegments[i][7]);
-			bRet	= true;
+		if (bUpdate) {
+			g_pHtml->CallJScript(
+				_T("SetNumericDisplay(%d,%.2f,%.2f,%.2f,%.2f,%.2f,%.2f,%.2f,%.2f)"), i, m_fSegments[i][0], m_fSegments[i][1], m_fSegments[i][2],
+				m_fSegments[i][3], m_fSegments[i][4], m_fSegments[i][5], m_fSegments[i][6], m_fSegments[i][7]);
+			bRet = true;
 		}
 	}
 
 	// mid
 	{
-		ACCUMULATE_DATA	post, recv;
-		post.m	= m_pNum->mid.post.m;
-		recv.m	= m_pNum->mid.recv.m;
-		DWORD	dwClock	= post.clk - recv.clk;
-		DWORD	dwAcc	= post.acc - recv.acc;
-		m_pNum->mid.recv.m	= post.m;
+		ACCUMULATE_DATA post, recv;
+		post.m			   = m_pNum->mid.post.m;
+		recv.m			   = m_pNum->mid.recv.m;
+		DWORD dwClock	   = post.clk - recv.clk;
+		DWORD dwAcc		   = post.acc - recv.acc;
+		m_pNum->mid.recv.m = post.m;
 
-		if(!dwClock) return false;
+		if (!dwClock)
+			return false;
 
-		float	val	= (double)dwAcc	/ dwClock;
+		float val = (double)dwAcc / dwClock;
 
-		if(val != m_fMid) {
+		if (val != m_fMid) {
 			g_pHtml->CallJScript(_T("SetNumericDisplay(4,0,0,0,0,0,0,0,%.2f)"), val);
-			m_fMid	= val;
-			bRet	= true;
+			m_fMid = val;
+			bRet   = true;
 		}
 	}
 	return bRet;
 }
-
